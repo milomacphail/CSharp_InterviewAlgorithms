1b50804e451dff19f6dae8a34f2c5f1ac35f62bc
milomacphail <milomacphail@gmail.com>
2019-12-03 13:48:59 -0500

    Linear search functions

--- Ch06/06_02/Begin/LinearSearch/Program.cs
+++ Ch06/06_02/Begin/LinearSearch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+using System;
 
 namespace LinearSearch
 {
@@ -6,7 +7,27 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            //TODO
+            int value = 7;
+            int[] sampleArray = new[] { 1, 3, 5, 7, 9, 11 };
+
+            System.Console.WriteLine("Our array contains:  ");
+            Array.ForEach(sampleArray, x => Write(x + " "));
+
+            Write($"\n\nThe result of the linear search for " + value + " is ");
+            WriteLine(linearSearch(sampleArray, value) + ".");
+        }
+
+        static int linearSearch(int[] array, int targetValue)
+        { 
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == targetValue)
+                {
+                   return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
