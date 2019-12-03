f5295f5f27dfd20e23e91a6d3ed9372ec8c2a481
milomacphail <milomacphail@gmail.com>
2019-12-03 13:25:45 -0500

    Binary Search functions work

--- Ch05/05_02/Begin/BinarySearch/Program.cs
+++ Ch05/05_02/Begin/BinarySearch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+using System;
 
 namespace BinarySearch
 {
@@ -6,6 +7,37 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int targetValue = 43;
+
+            int[] sampleArray = new int[] { 11, 23, 36, 43, 75, 81, 89 };
+
+            WriteLine("Our array contains: ");
+            Array.ForEach(sampleArray, x => Write(x + " "));
+
+            Write($"\n\nThe result of the binary search for " + targetValue + " is: ");
+            WriteLine(binarySearch(sampleArray, targetValue));
+        }
+
+        public static int binarySearch(int[] array, int target)
+        {
+            int firstIndex = 0;
+            int lastIndex = array.Length - 1;
+
+            while(firstIndex <= r)
+            {
+                int midpoint = (firstIndex + lastIndex) / 2;
+
+                if (target < array[midpoint])
+                {
+                    lastIndex = midpoint - 1;
+                }
+                else if (target > array[midpoint])
+                {
+                    firstIndex = midpoint + 1;
+                }
+                else return midpoint;
+            }
+            return -1;
         }
     }
 }
