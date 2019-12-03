065c25263a7be2cc21b448ce69d92b4d8dd22870
milomacphail <milomacphail@gmail.com>
2019-12-03 09:44:30 -0500

    Stack functions correctly

--- Ch03/03_02/Begin/Stack/Program.cs
+++ Ch03/03_02/Begin/Stack/Program.cs
@@ -13,4 +13,61 @@ namespace Stack
             //TODO
         }
     }
+
+    public class Stack
+    {
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new string[maxSize];
+            top = -1;
+        }
+
+        public void push(string m)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack is full!");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = m;
+            }
+        }
+
+        public string pop()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("Stack is empty!");
+                return "--";
+            }
+            else
+            {
+                int old_top = top;
+                top--;
+                return stackArray[old_top];
+            }
+        }
+
+        public string peek()
+        {
+            return stackArray[top];
+        }
+
+        private bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        private bool isFull()
+        {
+            return (maxSize - 1 == top);
+        }
+    }
 }
--- Ch03/03_03/Begin/Stack/Program.cs
+++ Ch03/03_03/Begin/Stack/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using static System.Console;
+using System.Threading.Tasks;
 
 namespace Stack
 {
@@ -10,11 +10,24 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            //TODO
+            Stack theStack = new Stack(4);
+            theStack.push("Avengers");
+            theStack.push("Star Wars");
+            theStack.push("Jaws");
+            theStack.push("Alien");
+            theStack.push("Alien 2");
+
+            System.Console.WriteLine("============ \nThe Stack contains:\n");
+            while(!theStack.isEmpty())
+            {
+                string movie = theStack.pop();
+                System.Console.WriteLine(movie);
+            }
         }
     }
 
-    public class Stack {
+    public class Stack
+    {
         private int maxSize;
         private string[] stackArray;
         private int top;
@@ -26,36 +39,45 @@ namespace Stack
             top = -1;
         }
 
-        public void push(string m) {
+        public void push(string m)
+        {
             if (isFull())
             {
-                WriteLine("This stack is full");
+                Console.WriteLine("Stack is full!");
             }
-            else {
+            else
+            {
                 top++;
                 stackArray[top] = m;
             }
         }
 
-        public string pop() {
+        public string pop()
+        {
             if (isEmpty())
             {
-                WriteLine("The stack is empty.");
+                Console.WriteLine("Stack is empty!");
                 return "--";
             }
-            else {
+            else
+            {
                 int old_top = top;
                 top--;
                 return stackArray[old_top];
             }
         }
 
-        private bool isEmpty()
+        public string peek()
+        {
+            return stackArray[top];
+        }
+
+        public bool isEmpty()
         {
             return (top == -1);
         }
 
-        private bool isFull()
+        public bool isFull()
         {
             return (maxSize - 1 == top);
         }
